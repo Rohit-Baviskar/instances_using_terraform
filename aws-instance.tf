83b059ea80530bdde3eba4eb14726e03734ac24b
Rohit-Baviskar <90634897+Rohit-Baviskar@users.noreply.github.com>
2023-04-21 14:22:23 +0530

    Update aws-instance.tf

--- aws-instance.tf
+++ aws-instance.tf
@@ -1,4 +1,28 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
-   instance_type = "t2.micro"
- }
+resource "aws_instance" "ubuntu" {
+  ami           = "${lookup(var.ami_id, var.region)}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-1"
+  }
+}
+
+
+resource "aws_instance" "ec2-user" {
+  ami           = "${lookup(var.ami_id, var.region)}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-2"
+  }
+}
+
+
+resource "aws_instance" "redhat" {
+  ami           = "${lookup(var.ami_id, var.region)}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-3"
+  }
+}
