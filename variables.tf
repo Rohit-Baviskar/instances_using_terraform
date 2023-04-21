7908c3d4a0812e402ff81aa0ffa7e6303c5bde48
Rohit-Baviskar <90634897+Rohit-Baviskar@users.noreply.github.com>
2023-04-21 11:44:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  type = "map"
+
+  default = {
+    ap-south-1    = "ami-02eb7a4783e7e9317"
+    ap-south-1    = "ami-0c768662cc797cd75"
+    ap-south-1    = "ami-0fdea1353c525c182"
+  }
+}
